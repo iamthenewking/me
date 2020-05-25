9b74da2c062769735f7f64804d11847f542a8c79
jeffrey gaboni <mrgabonijeff@gmail.com>
2020-05-25 08:58:30 +0800

    Update SocialMedia.vue

--- components/SocialMedia.vue
+++ components/SocialMedia.vue
@@ -1,30 +1,11 @@
 <template>
   <div class="social columns is-mobile">
     <div class="column has-text-centered">
-      <a title="Facebook" href="https://facebook.com/tuttiquintella">
+      <a title="Facebook" href="https://facebook.com/iamthenewking">
         <img src="img/facebook.svg" alt="Facebook" />
       </a>
     </div>
-    <div class="column has-text-centered">
-      <a title="Twitter" href="https://twitter.com/tuttiq">
-        <img className="fas fa-lg" src="img/twitter.svg" alt="Twitter" />
-      </a>
-    </div>
-    <div class="column has-text-centered">
-      <a title="Instagram" href="https://instagram.com/tuttiq.me">
-        <img src="img/instagram.svg" alt="Instagram" />
-      </a>
-    </div>
-    <div class="column has-text-centered">
-      <a title="LinkedIn" href="https://linkedin.com/in/tuttiq">
-        <img src="img/linkedin.svg" alt="LinkedIn" />
-      </a>
-    </div>
-    <div class="column has-text-centered">
-      <a title="Github" href="https://github.com/tuttiq">
-        <img src="img/github.svg" alt="Github" />
-      </a>
-    </div>
+   
   </div>
 </template>
 
